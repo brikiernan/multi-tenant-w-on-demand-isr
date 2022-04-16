f0a12d7a70f73a7f72562f9a6be1b277e7e1c2e5
Brian <brian@briankiernan.com>
2022-04-16 13:23:50 -0400

    update middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,25 +4,31 @@ const prod = process.env.NODE_ENV === 'production';
 const hostname = prod ? 'localhost:3000' : 'localhost:3000';
 
 const middleware: NextMiddleware = req => {
-  const host = req.headers.get('host');
-  if (!host) return NextResponse.next();
-  const currentHost = host.replace('.' + hostname, '');
   const url = req.nextUrl.clone();
   const { pathname: path } = url;
-  // const revalidate = req.headers.get('x-prerender-revalidate');
-  // const isApi = path.startsWith('/api');
-  // const isPublic = path.includes('.');
-  // const isVercel = host.includes('vercel');
+
+  const revalidate = req.headers.get('x-prerender-revalidate');
+  if (!!revalidate) {
+    url.pathname = `/_sites${path}`;
+    return NextResponse.rewrite(url);
+  }
+
+  const host = req.headers.get('host');
+  if (!host) return NextResponse.next();
+
+  const isApi = path.startsWith('/api');
+  const isPublic = path.includes('.');
+  const isVercel = host.includes('vercel');
+  if (host === hostname || isApi || isPublic || isVercel) {
+    return NextResponse.next();
+  }
 
   if (path.startsWith('/_sites')) {
     url.pathname = '/404';
     return NextResponse.rewrite(url);
   }
 
-  if (host === hostname) {
-    return NextResponse.next();
-  }
-
+  const currentHost = host.replace('.' + hostname, '');
   url.pathname = `/_sites/${currentHost}${path}`;
   return NextResponse.rewrite(url);
 };
